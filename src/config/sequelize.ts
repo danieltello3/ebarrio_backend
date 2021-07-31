d85e533429ef7ac015e9b912c189f20e3962225c
Daniel Tello <dantello.319@gmail.com>
2021-07-30 22:29:17 -0500

    se agrego ssl rejectunauthorized

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -5,6 +5,14 @@ dotenv.config();
 
 export const connection = new Sequelize(String(process.env.DATABASE_URL), {
    dialect: "postgres",
+   dialectOptions:
+      process.env.NODE_ENV != "production"
+         ? {}
+         : {
+              ssl: {
+                 rejectUnauthorized: false,
+              },
+           },
    timezone: "-05:00",
    logging: false,
 });
