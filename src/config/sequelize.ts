ff143bbd0b2d7b2ff7ff988f28088ae4307845ed
Daniel Tello <dantello.319@gmail.com>
2021-07-30 22:26:30 -0500

    se agrego problema de variable de entorno database

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connection = new Sequelize(String(process.env.DB_URL), {
+export const connection = new Sequelize(String(process.env.DATABASE_URL), {
    dialect: "postgres",
    timezone: "-05:00",
    logging: false,
